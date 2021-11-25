d051f1baaed2fde6e01626d86b3b080306f9b8d0
Parkduksung <duksung1234@naver.com>
2021-11-25 16:52:02 +0900

    [Jetpack Compose basics – 앱 스타일링 및 테마 적용]

--- week2/src/main/java/com/example/week2/MainActivity.kt
+++ week2/src/main/java/com/example/week2/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.week2.ui.theme.StudyComposeTheme
 
@@ -73,7 +74,7 @@ private fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold))
             }
             OutlinedButton(onClick = {
                 expanded = !expanded
