921289fa823009b40b0549989244e100ee4e4b0a
Parkduksung <duksung1234@naver.com>
2021-11-26 11:26:47 +0900

    [Layouts in Jetpack Compose – 목록 만들기] scroll 에 관한 내용.

--- week3/src/main/java/com/example/week3/MainActivity.kt
+++ week3/src/main/java/com/example/week3/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -101,7 +103,11 @@ fun LayoutsCodelab() {
 
         }
     ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp)
+        )
     }
 }
 
@@ -119,3 +125,39 @@ fun BodyContent(modifier: Modifier = Modifier) {
         Text(text = "Thanks for going through the Layouts codelab")
     }
 }
+
+
+/**
+ * Column은 기본적으로 스크롤은 다루지 않기 때문에, 몇몇 아이템은 화면 밖을 벗어나 보이지 않게된다.
+ * Modifier에 verticalScroll을 추가하여 Column내에서 스크롤을 활성화 할 수 있다.
+ */
+@Composable
+fun SimpleList() {
+    Column {
+        repeat(100) {
+            Text(text = "Item #$it")
+        }
+    }
+}
+
+/**
+ * Column은 모든 아이템들을 렌더링 한다. 심지어 화면에나오지 않는 아이템까지 렌더링 하려고 한다. 이런 점 때문에 성능 이슈가 발생하고, 목록의 사이즈가 커지면 커질수록 더욱 문제가 된다.
+ * 이런 문제를 회피하기 위해, LazyColumn을 사용한다. LazyColumn은 화면에 보이는 아이템만 렌더링한다. 이는 성능을 증대시키고, Modifier의 scroll항목을 사용할 필요가 없어진다.
+ * Note : Jetpack에 포함된 LazyColumn은 안드로이드 View의 RecyclerView와 동등하다.
+ * LazyColumn은 DSL을 제공하는데, 이를 통해 목록 내용을 묘사한다.
+ * 목록의 사이즈를 취하는 items라는 것을 사용할 것인데, 이는 배열 또는 리스트를 지원한다.
+ */
+
+@Composable
+fun LazyList() {
+    // 스크롤 위치를 저장한다. 이 state는 또한 스크롤을 이동할 때도 사용된다.
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(state = scrollState) {
+        items(100) {
+            Text(text = "Item #$it")
+        }
+    }
+}
+
+
