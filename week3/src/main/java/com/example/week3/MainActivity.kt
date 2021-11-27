22e9c2d13653286867ac5604d2455c12d43d984b
Parkduksung <duksung1234@naver.com>
2021-11-27 18:21:43 +0900

    [Layouts In Jetpack Compose – Intrinsics]

--- week3/src/main/java/com/example/week3/MainActivity.kt
+++ week3/src/main/java/com/example/week3/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StudyComposeTheme {
-                BodyContent()
+                TwoTexts(text1 = "Hi", text2 = "there")
             }
         }
     }
@@ -480,5 +480,32 @@ private fun decoupledConstraints(margin: Dp): ConstraintSet {
         constrain(text) {
             top.linkTo(button.bottom, margin)
         }
+    }
+}
+
+
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(
+            color = Color.Black, modifier = Modifier
+                .fillMaxHeight()
+                .width(1.dp)
+        )
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
     }
 }
