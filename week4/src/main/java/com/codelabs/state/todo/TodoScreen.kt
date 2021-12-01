ac351c9d4d9193fc7433d8391daa44adb1f5e192
Parkduksung <duksung1234@naver.com>
2021-12-01 14:53:32 +0900

    [Using state in Jetpack Compose – 상태 기반의 동적인 UI]

--- week4/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ week4/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -84,6 +84,13 @@ fun TodoRow(
 @Composable
 fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
     val (text, setText) = remember { mutableStateOf("") }
+    val (icon, setIcon) = remember { mutableStateOf(TodoIcon.Default)}
+    val iconsVisible = text.isNotBlank()
+    val submit = {
+        onItemComplete(TodoItem(text, icon))
+        setIcon(TodoIcon.Default)
+        setText("")
+    }
     Column {
         Row(Modifier
             .padding(horizontal = 16.dp)
@@ -94,17 +101,20 @@ fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
                 onTextChange = setText,
                 modifier = Modifier
                     .weight(1f)
-                    .padding(end = 8.dp)
+                    .padding(end = 8.dp),
+                onImeAction = submit
             )
             TodoEditButton(
-                onClick = {
-                    onItemComplete(TodoItem(text))
-                    setText("")
-                },
+                onClick = submit,
                 text = "Add",
                 modifier = Modifier.align(Alignment.CenterVertically),
                 enabled = text.isNotBlank()
             )
+        }
+        if (iconsVisible) {
+            AnimatedIconRow(icon, setIcon, Modifier.padding(top = 8.dp))
+        } else {
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
