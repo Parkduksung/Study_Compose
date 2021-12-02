ad32a64a42487670f71096c367957fd7b21da4f0
Parkduksung <duksung1234@naver.com>
2021-12-02 16:06:02 +0900

    [Testing in Jetpack Compose – 간단한 UI 테스트 만들기]

---
+++ week8/src/androidTest/java/com/example/week8/rally/TopAppBarTest.kt
@@ -0,0 +1,33 @@
+package com.example.week8.rally
+
+import androidx.compose.ui.test.assertIsSelectable
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import com.example.week8.RallyScreen
+import com.example.week8.ui.components.RallyTopAppBar
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+
+    @Test
+    fun rallyTopAppBarTest() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule.onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+
+}
