3d082253869b1de9a4697fd8a824ddb88e7b39b5
Parkduksung <duksung1234@naver.com>
2021-12-02 16:22:32 +0900

    [Testing in Jetpack Compose – 동기화]

---
+++ week8/src/androidTest/java/com/example/week8/rally/OverviewScreenTest.kt
@@ -0,0 +1,25 @@
+package com.example.week8.rally
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.example.week8.ui.overview.OverviewBody
+import org.junit.Rule
+import org.junit.Test
+
+class OverviewScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun overviewScreen_alertsDisplayed() {
+        composeTestRule.setContent {
+            OverviewBody()
+        }
+
+        composeTestRule
+            .onNodeWithText("Alerts")
+            .assertIsDisplayed()
+    }
+}
