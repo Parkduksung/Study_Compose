410f474d08858803b70fff9a49066da44237d006
Parkduksung <duksung1234@naver.com>
2021-12-02 11:39:37 +0900

    [Jetpack Compose Navigation – 딥 링크 지원 활성화하기]

--- week7/src/main/java/com/example/week7/MainActivity.kt
+++ week7/src/main/java/com/example/week7/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.example.week7.data.UserData
 import com.example.week7.ui.accounts.AccountsBody
 import com.example.week7.ui.accounts.SingleAccountBody
@@ -93,7 +94,10 @@ fun RallyApp() {
                             // Make argument type safe
                             type = NavType.StringType
                         }
-                    )
+                    ),
+                    deepLinks =  listOf(navDeepLink {
+                        uriPattern = "rally://$accountsName/{name}"
+                    })
                 ) { entry ->
                     val accountName = entry.arguments?.getString("name")
                     val account = UserData.getAccount(accountName)
