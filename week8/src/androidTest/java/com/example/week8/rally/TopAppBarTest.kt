7e7fbad00ec6e8017b5bb8d0a52ceabf465bc7b0
Parkduksung <duksung1234@naver.com>
2021-12-02 16:11:04 +0900

    [Testing in Jetpack Compose – 디버깅 테스트]

--- week8/src/androidTest/java/com/example/week8/rally/TopAppBarTest.kt
+++ week8/src/androidTest/java/com/example/week8/rally/TopAppBarTest.kt
@@ -1,13 +1,13 @@
 package com.example.week8.rally
 
-import androidx.compose.ui.test.assertIsSelectable
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
 import com.example.week8.RallyScreen
 import com.example.week8.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
+import java.util.*
 
 class TopAppBarTest {
 
@@ -30,4 +30,22 @@ class TopAppBarTest {
             .assertIsSelected()
     }
 
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        //전체 트리 보는것.
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists()
+    }
 }
