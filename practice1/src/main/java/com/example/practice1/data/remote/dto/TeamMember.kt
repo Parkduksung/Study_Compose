bdb92de6d995b715f71793e8f57291cc9d4a25a6
Parkduksung <duksung1234@naver.com>
2021-12-06 22:59:24 +0900

    [feature] coins api 추가.

---
+++ practice1/src/main/java/com/example/practice1/data/remote/dto/TeamMember.kt
@@ -0,0 +1,8 @@
+package com.example.practice1.data.remote.dto
+
+
+data class TeamMember(
+    val id: String,
+    val name: String,
+    val position: String
+)
---
+++ practice1/src/main/java/com/example/practice1/data/remote/dto/Whitepaper.kt
@@ -0,0 +1,7 @@
+package com.example.practice1.data.remote.dto
+
+
+data class Whitepaper(
+    val link: String,
+    val thumbnail: String
+)
