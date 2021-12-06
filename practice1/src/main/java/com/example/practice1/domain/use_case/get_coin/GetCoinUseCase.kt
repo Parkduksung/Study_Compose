186b4364b67ad98a162c787dd2ec555db94da5e6
Parkduksung <duksung1234@naver.com>
2021-12-06 23:58:37 +0900

    [feature] 변수명 수정.

--- practice1/src/main/java/com/example/practice1/domain/use_case/get_coin/GetCoinUseCase.kt
+++ practice1/src/main/java/com/example/practice1/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -1,8 +1,8 @@
 package com.example.practice1.domain.use_case.get_coin
 
 import com.example.practice1.common.Resource
-import com.example.practice1.data.remote.dto.toCoinDetail
-import com.example.practice1.domain.model.CoinDetail
+import com.example.practice1.data.remote.dto.toCoin
+import com.example.practice1.domain.model.Coin
 import com.example.practice1.domain.repository.CoinRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -13,15 +13,15 @@ import javax.inject.Inject
 class GetCoinUseCase @Inject constructor(
     private val coinRepository: CoinRepository
 ) {
-    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
         try {
-            emit(Resource.Loading<CoinDetail>())
-            val coin = coinRepository.getCoinById(coinId = coinId).toCoinDetail()
-            emit(Resource.Success<CoinDetail>(coin))
+            emit(Resource.Loading<List<Coin>>())
+            val coins = coinRepository.getCoins().map { it.toCoin() }
+            emit(Resource.Success<List<Coin>>(coins))
         } catch (e: HttpException) {
-            emit(Resource.Error<CoinDetail>(e.localizedMessage ?: "An unexpected error occured"))
+            emit(Resource.Error<List<Coin>>(e.localizedMessage ?: "An unexpected error occured"))
         } catch (e: IOException) {
-            emit(Resource.Error<CoinDetail>("Couldn't reach server. Check your internet connection."))
+            emit(Resource.Error<List<Coin>>("Couldn't reach server. Check your internet connection."))
         }
     }
 }
