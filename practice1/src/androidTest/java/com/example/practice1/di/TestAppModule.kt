3604cea215600d390c65e4cac73ff4ff11413d02
Parkduksung <duksung1234@naver.com>
2021-12-08 23:49:36 +0900

    [feature] TestAppModule 추가.

---
+++ practice1/src/androidTest/java/com/example/practice1/di/TestAppModule.kt
@@ -0,0 +1,35 @@
+package com.example.practice1.di
+
+import com.example.practice1.common.Constants
+import com.example.practice1.data.remote.CoinPaprikaApi
+import com.example.practice1.data.repository.CoinRepositoryImpl
+import com.example.practice1.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+}
