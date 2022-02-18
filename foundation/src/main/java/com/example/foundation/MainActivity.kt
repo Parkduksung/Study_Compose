15c321bf35c5a9de3b0cd88d67fe8eda6654841c
Parkduksung <duksung1234@naver.com>
2022-02-18 16:53:53 +0900

    [feature] box 에서 간단한 align

--- foundation/src/main/java/com/example/foundation/MainActivity.kt
+++ foundation/src/main/java/com/example/foundation/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.foundation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.foundation.ui.theme.StudyComposeTheme
 
@@ -17,7 +21,7 @@ class MainActivity : ComponentActivity() {
             StudyComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    BoxExample()
                 }
             }
         }
@@ -34,5 +38,16 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     StudyComposeTheme {
         Greeting("Android")
+    }
+}
+
+
+//Modifier.fillMaxWidth(Float.MAX_VALUE).fillMaxHeight(Float.MAX_VALUE) = fillMaxSize(Float.MAX_VALUE)
+@Composable
+fun BoxExample() {
+    Box(Modifier.fillMaxSize(Float.MAX_VALUE)) {
+        Text("안녕하세요.", modifier = Modifier.align(Alignment.Center))
+        Text("안녕하세요.", modifier = Modifier.align(Alignment.TopCenter))
+        Text("안녕하세요.", modifier = Modifier.align(Alignment.BottomCenter))
     }
 }
