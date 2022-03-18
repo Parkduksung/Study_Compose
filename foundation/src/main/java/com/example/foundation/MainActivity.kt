8b0733ad57d85dd8384a10299276ef203ff47963
Parkduksung <duksung1234@naver.com>
2022-03-18 15:16:58 +0900

    [feature] 화면전환 구현.

--- foundation/src/main/java/com/example/foundation/MainActivity.kt
+++ foundation/src/main/java/com/example/foundation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.foundation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
@@ -12,6 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.foundation.ui.theme.StudyComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,12 +26,31 @@ class MainActivity : ComponentActivity() {
             StudyComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    BoxExample()
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.ListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.ListScreen.route
+                        ) {
+                            ListScreen(navController = navController)
+                        }
+
+                        composable(
+                            route = Screen.DetailScreen.route
+                        ) {
+                            DetailScreen(navController = navController)
+                        }
+                    }
+
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String) {
@@ -49,5 +73,29 @@ fun BoxExample() {
         Text("안녕하세요.", modifier = Modifier.align(Alignment.Center))
         Text("안녕하세요.", modifier = Modifier.align(Alignment.TopCenter))
         Text("안녕하세요.", modifier = Modifier.align(Alignment.BottomCenter))
+    }
+}
+
+
+@Composable
+fun ListScreen(
+    navController: NavController
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text("ListScreen.", modifier = Modifier
+            .align(Alignment.Center)
+            .clickable(onClick = {
+                navController.navigate(Screen.DetailScreen.route)
+            })
+        )
+    }
+}
+
+@Composable
+fun DetailScreen(
+    navController: NavController
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text("DetailScreen.", modifier = Modifier.align(Alignment.Center))
     }
 }
