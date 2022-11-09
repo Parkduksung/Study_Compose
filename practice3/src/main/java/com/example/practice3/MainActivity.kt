7075e29e3c43c48e130536a4afc7762721791429
Parkduksung <duksung1234@naver.com>
2022-11-09 23:09:03 +0900

    [feature]serialize json file

--- practice3/src/main/java/com/example/practice3/MainActivity.kt
+++ practice3/src/main/java/com/example/practice3/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.practice3
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.practice3.ui.theme.StudyComposeTheme
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +22,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             StudyComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
                     Greeting("Android")
+                    getLOLChamp()
                 }
             }
         }
     }
+
+    private fun getLOLChamp() {
+        val jsonString = assets.open("LOLChamp.json").bufferedReader().use { it.readText() }
+
+        val json = Json {
+            ignoreUnknownKeys = true
+            prettyPrint = true
+        }
+
+        val lolResponseFromJsonString = json.decodeFromString<LOLResponse<LOLChamp>>(jsonString)
+
+        Log.d("결과", lolResponseFromJsonString.toString())
+
+    }
+
 }
 
 @Composable
@@ -38,3 +61,13 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
+
+@Serializable
+data class LOLResponse<T>(val type: String, val version: String, val data: Map<String, T>)
+
+@Serializable
+data class LOLChamp(
+    val id: String,
+    val name: String
+)
