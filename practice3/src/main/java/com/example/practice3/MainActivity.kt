6f76b79032d6def85b7119b1a16593be1a2a50cd
Parkduksung <duksung1234@naver.com>
2022-11-10 11:53:08 +0900

    [feature]retrofit 적용.

--- practice3/src/main/java/com/example/practice3/MainActivity.kt
+++ practice3/src/main/java/com/example/practice3/MainActivity.kt
@@ -12,9 +12,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.practice3.ui.theme.StudyComposeTheme
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.http.GET
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,17 +43,31 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
     private fun getLOLChamp() {
-        val jsonString = assets.open("LOLChamp.json").bufferedReader().use { it.readText() }
 
-        val json = Json {
-            ignoreUnknownKeys = true
-            prettyPrint = true
+        CoroutineScope(Dispatchers.IO).launch {
+            val json = Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+            }
+
+            val retrofit = Retrofit.Builder().baseUrl("https://ddragon.leagueoflegends.com/")
+                .addConverterFactory(json.asConverterFactory(MediaType.parse("application/json")!!))
+                .build()
+
+
+            val service = retrofit.create(LOLService::class.java)
+
+            val call = service.getLOLResponse()
+
+            val response = call.execute()
+
+            if(response.isSuccessful){
+                val lolResponse = response.body()
+                Log.d("결과", lolResponse.toString())
+            }
         }
-
-        val lolResponseFromJsonString = json.decodeFromString<LOLResponse<LOLChamp>>(jsonString)
-
-        Log.d("결과", lolResponseFromJsonString.toString())
 
     }
 
@@ -69,5 +93,13 @@ data class LOLResponse<T>(val type: String, val version: String, val data: Map<S
 @Serializable
 data class LOLChamp(
     val id: String,
-    val name: String
+    val name: String,
+    val imageUrl: String = name + "_0.jpg"
 )
+
+
+interface LOLService {
+
+    @GET("cdn/11.16.1/data/en_US/champion.json")
+    fun getLOLResponse(): Call<LOLResponse<LOLChamp>>
+}
