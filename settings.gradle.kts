23fe324df9e6513d5bbd27106666fe84fbea45a4
Parkduksung <duksung1234@naver.com>
2022-12-21 23:30:26 +0900

    [feature] settings.gradle => settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         gradlePluginPortal()
         google()
@@ -16,18 +17,18 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "StudyCompose"
-include ':app'
-include ':week1'
-include ':week2'
-include ':week3'
-include ':week4'
-include ':week5'
-include ':week6'
-include ':week7'
-include ':week8'
-include ':practice1'
-include ':foundation'
-include ':practice2'
-include ':part1'
-include ':practice3'
-include ':book1'
+include(":app")
+include(":week1")
+include(":week2")
+include(":week3")
+include(":week4")
+include(":week5")
+include(":week6")
+include(":week7")
+include(":week8")
+include(":practice1")
+include(":foundation")
+include(":practice2")
+include(":part1")
+include(":practice3")
+include(":book1")
