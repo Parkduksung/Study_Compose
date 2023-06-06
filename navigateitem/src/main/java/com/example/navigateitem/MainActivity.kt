d97b251b3fc391fc4fb93b020593406ba142283a
Parkduksung <duksung1234@naver.com>
2023-06-06 21:31:29 +0900

    previousBackStackEntry 이용한 전달.

--- navigateitem/src/main/java/com/example/navigateitem/MainActivity.kt
+++ navigateitem/src/main/java/com/example/navigateitem/MainActivity.kt
@@ -6,16 +6,26 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -26,6 +36,9 @@ import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.navigateitem.ui.theme.StudyComposeTheme
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonAdapter
@@ -41,55 +54,124 @@ import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
-    private val customDataStore by lazy { CustomDataStore(this) }
+    private val listAdapter: JsonAdapter<List<CustomItem>> by lazy {
+        moshi.adapter(Types.newParameterizedType(List::class.java, CustomItem::class.java))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StudyComposeTheme {
-                // A surface container using the 'background' color from the theme
 
+                val navHostController = rememberNavController()
 
-                val list = customDataStore.customListFlow.collectAsState(initial = emptyList())
+                NavHost(navController = navHostController, startDestination = "screen1") {
+                    composable("screen1") { entry ->
 
-                val scope = rememberCoroutineScope()
+                        val text = entry.savedStateHandle.get<String>("input")
 
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
+                        LazyColumn(modifier = Modifier.fillMaxSize()) {
 
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-
-                        Button(onClick = {
-                            Log.d("결과", list.value.toString())
-                        }) {
-                            Text(text = "Get")
-                        }
-
-                        Spacer(modifier = Modifier.height(10.dp))
-
-                        Button(onClick = {
-
-                            CoroutineScope(Dispatchers.IO).launch {
-                                val customList = mutableListOf<CustomItem>().apply {
-                                    (1..10).forEach {
-                                        add(CustomItem(it, "name$it"))
+                            item {
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(100.dp)
+                                ) {
+                                    Button(modifier = Modifier.align(Alignment.Center), onClick = {
+                                        navHostController.navigate("screen2")
+                                    }) {
+                                        Text(text = "Route Screen2")
                                     }
                                 }
-
-                                customDataStore.setCustomList(emptyList())
                             }
-                        }) {
-                            Text(text = "Save")
+
+                            text?.let {
+                                items(listAdapter.fromJson(it)!!) {
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .height(50.dp)
+                                    ) {
+                                        Text(
+                                            text = it.name,
+                                            modifier = Modifier.align(Alignment.Center)
+                                        )
+                                    }
+                                }
+                            }
                         }
                     }
 
+                    composable("screen2") {
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            Button(modifier = Modifier.align(Alignment.Center), onClick = {
+                                val dataList = listOf(
+                                    CustomItem(1, "1번"),
+                                    CustomItem(2, "2번"),
+                                    CustomItem(3, "3번"),
+                                    CustomItem(4, "4번"),
+                                    CustomItem(5, "5번"),
+                                    CustomItem(6, "6번"),
+                                    CustomItem(7, "7번"),
+                                    CustomItem(8, "8번"),
+                                    CustomItem(9, "9번"),
+                                    CustomItem(10, "10번"),
+                                    CustomItem(11, "11번"),
+                                    CustomItem(12, "12번"),
+                                    CustomItem(13, "13번"),
+                                    CustomItem(14, "14번"),
+                                    CustomItem(15, "15번"),
+                                    CustomItem(16, "16번"),
+                                    CustomItem(17, "17번"),
+                                    CustomItem(18, "18번"),
+                                    CustomItem(19, "19번"),
+                                    CustomItem(20, "20번"),
+                                    CustomItem(21, "21번"),
+                                    CustomItem(22, "22번"),
+                                    CustomItem(23, "23번"),
+                                    CustomItem(24, "24번"),
+                                    CustomItem(25, "25번"),
+                                    CustomItem(26, "26번"),
+                                    CustomItem(27, "27번"),
+                                    CustomItem(28, "28번"),
+                                    CustomItem(29, "29번"),
+                                    CustomItem(30, "30번"),
+                                    CustomItem(31, "31번"),
+                                    CustomItem(32, "32번"),
+                                    CustomItem(33, "33번"),
+                                    CustomItem(34, "34번"),
+                                    CustomItem(35, "35번"),
+                                    CustomItem(36, "36번"),
+                                    CustomItem(37, "37번"),
+                                    CustomItem(38, "38번"),
+                                    CustomItem(39, "39번"),
+                                    CustomItem(40, "40번"),
+                                    CustomItem(41, "41번"),
+                                    CustomItem(42, "42번"),
+                                    CustomItem(43, "43번"),
+                                    CustomItem(44, "44번"),
+                                    CustomItem(45, "45번"),
+                                    CustomItem(46, "46번"),
+                                    CustomItem(47, "47번"),
+                                    CustomItem(48, "48번"),
+                                    CustomItem(49, "49번"),
+                                    CustomItem(50, "50번"),
+                                )
+
+                                navHostController.previousBackStackEntry?.savedStateHandle?.set(
+                                    "input",
+                                    listAdapter.toJson(dataList)
+                                )
+
+                                navHostController.popBackStack()
+                            }) {
+                                Text(text = "Send Screen1")
+                            }
+                        }
+                    }
                 }
             }
         }
