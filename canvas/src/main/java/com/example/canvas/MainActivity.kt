fdd865e23af8ca4d9fd231a00fff063fcd0244cf
Parkduksung <duksung1234@naver.com>
2023-06-07 21:19:30 +0900

    canvas practice 1

---
+++ canvas/src/main/java/com/example/canvas/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.canvas
+
+import android.graphics.Typeface
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.toColor
+import com.example.canvas.ui.theme.StudyComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StudyComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+//insta logo
+@Preview(showBackground = true)
+@Composable
+fun CanvasPractice1(modifier: Modifier = Modifier) {
+    val instaColors = listOf(Color.Yellow, Color.Red, Color.Magenta)
+
+    Canvas(modifier = modifier
+        .size(100.dp)
+        .padding(16.dp), onDraw = {
+
+        drawRoundRect(
+            brush = Brush.linearGradient(instaColors),
+            cornerRadius = CornerRadius(60f, 60f),
+            style = Stroke(width = 15f, cap = StrokeCap.Round)
+        )
+
+        drawCircle(
+            brush = Brush.linearGradient(instaColors),
+            radius = 45f,
+            style = Stroke(width = 15f, cap = StrokeCap.Round)
+        )
+        drawCircle(
+            brush = Brush.linearGradient(instaColors),
+            radius = 13f,
+            center = Offset(this.size.width * 0.8f, this.size.height * 0.2f)
+        )
+    })
+}
+
