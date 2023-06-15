45d713e8b55c217605c940a542000cdd61111e21
Parkduksung <duksung1234@naver.com>
2023-06-16 00:24:57 +0900

    높이 체크

--- keyboard/src/main/java/com/example/keyboard/MainActivity.kt
+++ keyboard/src/main/java/com/example/keyboard/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.keyboard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.keyboard.ui.theme.StudyComposeTheme
+import com.google.accompanist.insets.LocalWindowInsets
+import com.google.accompanist.insets.rememberInsetsPaddingValues
+import com.google.accompanist.insets.toPaddingValues
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +39,44 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CheckKeyboardHeight()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun CheckKeyboardHeight() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StudyComposeTheme {
-        Greeting("Android")
+
+    val imePadding = LocalWindowInsets.current.ime.toPaddingValues()
+
+
+    var inputString by remember { mutableStateOf("") }
+
+
+    Box(
+        modifier = Modifier
+            .padding(imePadding)
+            .fillMaxSize()
+    ) {
+        TextField(
+            value = inputString,
+            onValueChange = { inputString = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+                .align(
+                    Alignment.Center
+                )
+        )
     }
+
+    SideEffect {
+        val keyboardHeight = imePadding.calculateBottomPadding()
+        Log.d("결과", keyboardHeight.value.toString())
+    }
+
 }
