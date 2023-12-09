bcdcc90b1d43a4fd12a13ecf75ce360f726721c0
Parkduksung <duksung1234@naver.com>
2023-12-09 22:22:42 +0900

    loading 화면 추가.

--- widget/src/main/java/com/example/widget/CounterWidget.kt
+++ widget/src/main/java/com/example/widget/CounterWidget.kt
@@ -2,31 +2,23 @@ package com.example.widget
 
 import android.content.Context
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.action.ActionParameters
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.action.ActionCallback
-import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.provideContent
-import androidx.glance.appwidget.state.updateAppWidgetState
-import androidx.glance.background
-import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
-import androidx.glance.text.FontWeight
+import androidx.glance.layout.padding
 import androidx.glance.text.Text
-import androidx.glance.text.TextStyle
-import androidx.glance.unit.ColorProvider
 import com.example.widget.ui.theme.StudyComposeTheme
 
-object CounterWidget : GlanceAppWidget() {
+class CounterWidget : GlanceAppWidget() {
 
     val countKey = intPreferencesKey("count")
     override suspend fun provideGlance(context: Context, id: GlanceId) {
@@ -39,26 +31,20 @@ object CounterWidget : GlanceAppWidget() {
 
     @Composable
     private fun Widget() {
-        val count = currentState(key = countKey) ?: 0
         Column(
             modifier = GlanceModifier
-                .fillMaxSize()
-                .background(Color.DarkGray),
-            verticalAlignment = Alignment.Vertical.CenterVertically,
+                .fillMaxSize(),
+            verticalAlignment = Alignment.Vertical.Top,
             horizontalAlignment = Alignment.Horizontal.CenterHorizontally
         ) {
             Text(
-                text = count.toString(),
-                style = TextStyle(
-                    fontWeight = FontWeight.Medium,
-                    color = ColorProvider(Color.White),
-                    fontSize = 26.sp
-                )
+                text ="Hello Glance!",
+                modifier = GlanceModifier.padding(12.dp)
             )
-            Button(
-                text = "Inc",
-                onClick = actionRunCallback(IncrementActionCallback::class.java)
-            )
+//            Button(
+//                text = "Inc",
+//                onClick = actionRunCallback(IncrementActionCallback::class.java)
+//            )
         }
     }
 
@@ -68,7 +54,7 @@ object CounterWidget : GlanceAppWidget() {
 
 class SimpleCounterWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget
-        get() = CounterWidget
+        get() = CounterWidget()
 }
 
 class IncrementActionCallback : ActionCallback {
@@ -77,14 +63,14 @@ class IncrementActionCallback : ActionCallback {
         glanceId: GlanceId,
         parameters: ActionParameters
     ) {
-        updateAppWidgetState(context, glanceId) { prefs ->
-            val currentCount = prefs[CounterWidget.countKey]
-            if (currentCount != null) {
-                prefs[CounterWidget.countKey] = currentCount + 1
-            } else {
-                prefs[CounterWidget.countKey] = 1
-            }
-        }
-        CounterWidget.update(context, glanceId)
+//        updateAppWidgetState(context, glanceId) { prefs ->
+//            val currentCount = prefs[CounterWidget.countKey]
+//            if (currentCount != null) {
+//                prefs[CounterWidget.countKey] = currentCount + 1
+//            } else {
+//                prefs[CounterWidget.countKey] = 1
+//            }
+//        }
+//        CounterWidget.update(context, glanceId)
     }
 }
