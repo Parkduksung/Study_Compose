5af5437980ea4423cf0f937cfd8d7348841408b0
Parkduksung <duksung1234@naver.com>
2023-12-18 21:54:27 +0900

    색상은 Glance 전용.

--- widget/src/main/java/com/example/widget/WhatsAppGlanceWidgetReceiver.kt
+++ widget/src/main/java/com/example/widget/WhatsAppGlanceWidgetReceiver.kt
@@ -1,11 +1,16 @@
 package com.example.widget
 
 import android.content.Context
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.provideContent
+import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
 
 class WhatsAppGlanceWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = WhatsAppGlanceWidget()
@@ -16,6 +21,11 @@ class WhatsAppGlanceWidget : GlanceAppWidget() {
         provideContent {
             Text(
                 text = "Hello World!",
+                style = TextStyle(
+                    fontWeight = FontWeight.Medium,
+                    color = ColorProvider(Color.White),
+                    fontSize = 26.sp
+                )
             )
         }
     }
