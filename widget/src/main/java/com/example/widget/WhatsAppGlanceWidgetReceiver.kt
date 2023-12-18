d0acdced446aae6a27be821fd267c3102a0eb0b2
Parkduksung <duksung1234@naver.com>
2023-12-18 22:00:33 +0900

    glance Column, text, button

--- widget/src/main/java/com/example/widget/WhatsAppGlanceWidgetReceiver.kt
+++ widget/src/main/java/com/example/widget/WhatsAppGlanceWidgetReceiver.kt
@@ -1,13 +1,15 @@
 package com.example.widget
 
 import android.content.Context
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.provideContent
-import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
@@ -19,14 +21,20 @@ class WhatsAppGlanceWidgetReceiver : GlanceAppWidgetReceiver() {
 class WhatsAppGlanceWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
-            Text(
-                text = "Hello World!",
-                style = TextStyle(
-                    fontWeight = FontWeight.Medium,
-                    color = ColorProvider(Color.White),
-                    fontSize = 26.sp
+            var editing by remember { mutableStateOf(false) }
+            androidx.glance.layout.Column {
+                Text(
+                    text = if (editing) "Editing" else "Not Editing", style = TextStyle(
+                        color = ColorProvider(
+                            Color.Green
+                        )
+                    )
                 )
-            )
+                androidx.glance.Button(text = "Click Me", onClick = {
+                    editing = !editing
+                })
+            }
+
         }
     }
 
